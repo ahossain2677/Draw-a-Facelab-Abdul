7b0cb4b5cdd7f9f15ed8704bcd245cef20c14b86
BT_1N3_08@bktech.local <BT_1N3_08@bktech.local>
2018-11-29 08:41:45 -0500

    Initial commit

---
+++ src/FaceDrawing.java
@@ -0,0 +1,13 @@
+public class FaceDrawing
+{
+
+
+    private 
+    FaceDrawing.fill(String string );
+    this.String = String;
+
+    FaceDrawing.edit(char replace, int row , int column);
+    this.replace = replace;
+    this.row = row;
+    this.column= column;
+}
