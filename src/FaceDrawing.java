c5d9bc8fa40c208180cc37bd629a45df7f5f4b50
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2018-11-30 08:46:47 -0500

    Update FaceDrawing.java

--- src/FaceDrawing.java
+++ src/FaceDrawing.java
@@ -2,12 +2,16 @@ public class FaceDrawing
 {
 
 
-    private 
+    private fill{
     FaceDrawing.fill(String string );
     this.String = String;
-
+    }
+    
+    private edit{
     FaceDrawing.edit(char replace, int row , int column);
     this.replace = replace;
     this.row = row;
     this.column= column;
-}
+    }
+    
+    }
